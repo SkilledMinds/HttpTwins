b505e3572a6f95be7c23f765a3cb0b21df9661d1
gyan <gyanendraojhaa@gmail.com>
2025-08-03 22:29:07 +0530

    Initial version

---
+++ src/main/java/com/example/httpTwins/HttpTwinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.httpTwins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpTwinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpTwinsApplication.class, args);
+	}
+
+}
